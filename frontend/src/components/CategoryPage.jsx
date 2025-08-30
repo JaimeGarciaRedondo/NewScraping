5e64e4e180aaeaca0cb335f279f1cef48b618a5f
JaimeGarciaRedondo <jaimegare.contacto@gmail.com>
2025-08-30 15:43:32 +0200

    final

--- frontend/src/components/CategoryPage.jsx
+++ frontend/src/components/CategoryPage.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import NewsCard from "../components/NewsCard";
 
 export default function CategoryPage({ allNoticias }) {
-  const { categoria } = useParams();
+  const { categoria } = useParams();  
   const noticiasFiltradas = allNoticias.filter(
     (n) => n.categoria === categoria
   );
